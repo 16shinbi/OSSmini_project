29a44ca7cdcdaf030e559e0194b1319d1385c4c4
Lee shinbi <21600510@handong.edu>
2020-04-19 13:00:08 +0900

    Add searchscore

--- product.h
+++ product.h
@@ -22,5 +22,5 @@ int deleteProduct(Product *s);      //삭제하는 함수
 void saveData(Product *s[], int count);     //file저장하는 함수
 int loadData(Product *s[]);     //file불러오는 함수
 void searchName(Product *s[], int count);       //이름검색기능함수
-//void searchscore(Product *s[], int count);      //별점수검색기능함수
+void searchscore(Product *s[], int count);      //별점수검색기능함수
 //void searchPrice(Product *s[], int count);      //가격검색기능함수
