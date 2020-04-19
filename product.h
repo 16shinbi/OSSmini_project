d1e7af59745fb565785a93dcdbc4168b5a33da6e
Lee shinbi <21600510@handong.edu>
2020-04-19 12:20:25 +0900

    Add loaddata

--- product.h
+++ product.h
@@ -19,7 +19,7 @@ int updateProduct(Product *s);      //수정하는 함수
 void listProduct(Product *s[], int count);      //읽어온 데이터를 리스트로 출력하는 함수 
 int deleteProduct(Product *s);      //삭제하는 함수
 void saveData(Product *s[], int count);     //file저장하는 함수
-//int loadData(Product *s[]);     //file불러오는 함수
+int loadData(Product *s[]);     //file불러오는 함수
 //void searchName(Product *s[], int count);       //이름검색기능함수
 //void searchscore(Product *s[], int count);      //별점수검색기능함수
 //void searchPrice(Product *s[], int count);      //가격검색기능함수
